d8730c17262b486dce2105f4fd1e6928b7dd7217
Jeongbin Park <pjb7687@gmail.com>
2016-05-06 19:12:01 +0300

    Update install_depend_windows.sh

--- install_depend_windows.sh
+++ install_depend_windows.sh
@@ -1,24 +1,27 @@
 install_mingw_pkg() {
   echo "Downloading $package..."
   svn checkout https://github.com/Alexpux/MINGW-packages/trunk/mingw-w64-$package
+  
+  cd mingw-w64-$package
 
-  . mingw-w64-$package/PKGBUILD
+  . PKGBUILD
 
   mkdir src && srcdir=src
   
   cd src
   for f in ${source[@]}; do
-    curl -O $f
+    if [[ $f == *":"* ]]; then
+      curl -O $f
+    fi
   done
   
   echo "Decompressing $package..."
-  tar zxf `basename $source`
+  tar zxf `basename $source` # 1st file is to be decompressed
   
   cd ..
 
   echo "Preparing for $package..."
   libdir=/usr/${MINGW_CHOST}/lib
-  pwd
   prepare
   
   echo "Building $package..."
@@ -28,8 +31,8 @@ install_mingw_pkg() {
   pkgdir=/usr/${MINGW_CHOST}
   package
   
-  cd ..
   echo "Installed $package."
+  cd ..
 }
 
 MINGW_CHOST=i586-mingw32msvc
