6f16a86b98125bfee3b22af841581eeb0ac44403
Jeongbin Park <pjb7687@gmail.com>
2016-05-06 19:04:13 +0300

    Update install_depend_windows.sh

--- install_depend_windows.sh
+++ install_depend_windows.sh
@@ -1,30 +1,37 @@
-install_pkg() {
-  echo "Downloading $pkg_name..."
-  svn checkout https://github.com/Alexpux/MINGW-packages/trunk/mingw-w64-$pkg_name
-  . mingw-w64-$pkg_name/PKGBUILD
-  curl -O $source
-  source_fn=`basename $source`
-  source_dn=`basename $source_fn .tar.gz`
-  echo "Decompressing $pkg_name..."
-  tar zxf $source_fn
-  cd $source_dn
-  echo "../${source_dn}-mingw.patch"
-  ls ../${source_dn}-mingw.patch
-  if [[ -f ../${source_dn}-mingw.patch ]]; then
-    echo "Applying patch for $pkg_name..."
-    patch -p0 < ../${source_dn}-mingw.patch
-  fi
-  echo "Configuring $pkg_name..."
-  ./configure --prefix=/usr/${_toolchain}/ --host ${_toolchain}
-  echo "Building $pkg_name..."
-  make && make install
+install_mingw_pkg() {
+  echo "Downloading $package..."
+  svn checkout https://github.com/Alexpux/MINGW-packages/trunk/mingw-w64-$package
+
+  . mingw-w64-$package/PKGBUILD
+
+  mkdir src && srcdir=src
+  
+  cd src
+  for f in ${source[@]}; do
+    curl -O $f
+  done
+  
+  echo "Decompressing $package..."
+  tar zxf `basename $source`
+  
   cd ..
-  echo "Installed $pkg_name."
+
+  echo "Preparing for $package..."
+  libdir=/usr/${MINGW_CHOST}/lib
+  prepare
+  
+  echo "Building $package..."
+  build
+  
+  echo "Installing $package..."
+  pkgdir=/usr/${MINGW_CHOST}
+  package
+  
+  cd ..
+  echo "Installed $package."
 }
 
-_toolchain=i586-mingw32msvc
+MINGW_CHOST=i586-mingw32msvc
 
 mkdir depend_windows && cd depend_windows
-
-pkg_name=readline
-install_pkg
+package=readline && install_mingw_pkg
