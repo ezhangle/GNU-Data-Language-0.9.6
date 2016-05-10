fa04179fac836090655baf95750c3a2feac51c28
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 17:02:35 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -10,6 +10,8 @@ tar zxvf bsd-xdr-1.0.0.tar.gz
 cd bsd-xdr-1.0.0
 sed -i 's/%hh/%/g' src/test/test_data.c
 make
+mv mingw/libxdr.dll.a /usr/lib/
+mv rpc /usr/include/
 cd ..
 
 pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline mingw-w64-i686-libpng mingw-w64-i686-gsl mingw-w64-i686-cmake
