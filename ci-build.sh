ef95c1066190304f09ed0bf389e31cc9561941bf
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 17:04:53 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -5,13 +5,15 @@ set -e
 mkdir build
 cd build
 
+ls /
+
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bsd-xdr/bsd-xdr-1.0.0.tar.gz
 tar zxvf bsd-xdr-1.0.0.tar.gz
 cd bsd-xdr-1.0.0
 sed -i 's/%hh/%/g' src/test/test_data.c
 make
-mv mingw/libxdr.dll.a /lib/
-mv rpc /include/
+mv mingw/libxdr.dll.a /mingw/lib/
+mv rpc /mingw/include/
 cd ..
 
 pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline mingw-w64-i686-libpng mingw-w64-i686-gsl mingw-w64-i686-cmake
