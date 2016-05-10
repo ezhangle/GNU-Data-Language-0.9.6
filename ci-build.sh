78e62977d3ed7d3f48dc76cef37bdc67a4080b27
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 17:16:47 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -6,8 +6,8 @@ mkdir build
 cd build
 
 # wxWidgets
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.zip
-unzip wxWidgets-3.1.0.zip
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
+tar jxf wxWidgets-3.1.0.tar.bz2
 cd wxWidgets-3.1.0
 ls
 
