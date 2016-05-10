4a5abaf35da8489475d5b6d65875d14d7d368f7c
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 16:12:22 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -5,8 +5,6 @@ set -e
 if test "$platform" = "bash"; then
     echo "Building on Ubuntu Linux x86_64"
     sudo apt-get install build-essential automake
-elif test "$platform" = "msys32"; then
-    echo "Building on Msys2 i686"
 elif test "$platform" = "mingw32"; then
     echo "Building on MinGW-w64 i686"
     pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline
