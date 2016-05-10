0c38355203112a4527be2c57768611eade519bf9
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 16:53:30 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -5,18 +5,15 @@ set -e
 mkdir build
 cd build
 
-if test "$platform" = "bash"; then
-    echo "Building on Ubuntu Linux x86_64"
-    sudo apt-get install build-essential automake
-    cmake ..
-elif test "$platform" = "mingw32"; then
-    echo "Building on MinGW-w64 i686"
-    mv /usr/bin/sh.exe /usr/bin/sh-moved.exe
-    pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline mingw-w64-i686-libpng mingw-w64-i686-gsl mingw-w64-i686-cmake
-    cmake -G "MinGW Makefiles" ..
-else
-    echo "unknown environment!"
-    exit 1
-fi
+mv /usr/bin/sh.exe /usr/bin/sh-moved.exe
+
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bsd-xdr/bsd-xdr-1.0.0.tar.gz
+tar zxvf bsd-xdr-1.0.0.tar.gz
+cd bsd-xdr-1.0.0
+mingw32-make
+cd ..
+
+pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline mingw-w64-i686-libpng mingw-w64-i686-gsl mingw-w64-i686-cmake
+cmake -G "MinGW Makefiles" ..
 
 make
