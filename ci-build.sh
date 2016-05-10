266cbeeb64bdad915e8fb4db448719e6ca47f9a1
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 16:23:22 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -2,19 +2,21 @@
 
 set -e
 
+mkdir build
+cd build
+
 if test "$platform" = "bash"; then
     echo "Building on Ubuntu Linux x86_64"
     sudo apt-get install build-essential automake
+    cmake ..
 elif test "$platform" = "mingw32"; then
     echo "Building on MinGW-w64 i686"
     pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline
     pacman --sync --noconfirm --noprogressbar mingw-w64-i686-cmake
+    cmake -G "MinGW Makefiles" ..
 else
     echo "unknown environment!"
     exit 1
 fi
 
-mkdir build
-cd build
-cmake ..
 make
