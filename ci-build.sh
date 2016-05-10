822015494e1ebf7c74c377c3a5cfd74c699765bc
Jeongbin Park <pjb7687@gmail.com>
2016-05-10 16:57:00 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -10,7 +10,7 @@ mv /usr/bin/sh.exe /usr/bin/sh-moved.exe
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bsd-xdr/bsd-xdr-1.0.0.tar.gz
 tar zxvf bsd-xdr-1.0.0.tar.gz
 cd bsd-xdr-1.0.0
-/mingw32/bin/make
+make
 cd ..
 
 pacman --sync --noconfirm --noprogressbar mingw-w64-i686-readline mingw-w64-i686-libpng mingw-w64-i686-gsl mingw-w64-i686-cmake
